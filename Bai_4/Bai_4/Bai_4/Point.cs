4628ade91fbd142df502def6be5b1fd8f59116d8
Sơn Nguyễn <nbhs.it.98@gmail.com>
2019-10-12 19:34:59 +0700

    Son

---
+++ Bai_4/Bai_4/Bai_4/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Bai_4
+{
+    public class Point : Shape
+    {
+        public Point()
+        {
+        }
+
+        public override void draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bai_4/Bai_4/Bai_4/Shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Bai_4
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+
+        }
+   }
+
